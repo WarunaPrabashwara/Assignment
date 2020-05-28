33c06afb4716de5cb8f01a0d78207c2377818d7c
WarunaPrabashwara <55828398+WarunaPrabashwara@users.noreply.github.com>
2020-05-28 13:20:32 +0530

    Create 2.scala

---
+++ Assignment 2/2.scala
@@ -0,0 +1,38 @@
+object takehomesalary extends App{
+
+def best_profit( )  ={
+            For_100 :Int=100*(20-3)
+            For_120 :Int =120*(15-3)
+            For_140 :Int =140*(10-3)
+          
+             
+       if(cost(For_100) >cost(For_120) & & cost(For_100)>cost(For_140)){
+         print "best price is 20"
+       print ("profit from 100,120,140 people respectively" 
+            +cost(For_100)+ "," +
+          cost(For_120) + "," +
+           cost(For_140)) 
+              } 
+else{ if (cost(For_120)> cost(For_140)) { print "best ticket price is 15" 
+print ("profit from 100,120,140 people respectively" 
+            +cost(For_100)+ "," +
+          cost(For_120) + "," +
+           cost(For_140)) 
+   else{ print "best ticket price is 10"  
+print ("profit from 100,120,140 people respectively" 
+            +cost(For_100)+ "," +
+          cost(For_120) + "," +
+           cost(For_140)) 
+        } 
+
+def cost (k :Int) ={
+
+       return (k-500)
+     } 
+
+   
+
+
+
+best_profit() 
+}
