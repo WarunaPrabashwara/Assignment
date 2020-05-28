611121a4ab27f5ef4226544226ebda96e109d7de
WarunaPrabashwara <55828398+WarunaPrabashwara@users.noreply.github.com>
2020-05-28 12:46:54 +0530

    Update 1.scala

--- Assignment 2/1.scala
+++ Assignment 2/1.scala
@@ -1,1 +1,16 @@
+object takehomesalary extends App{
 
+def normal(hours :Int)Int ={
+           return (150*hours)
+                               } 
+
+def ot(hours : Int)Int ={
+               return (75*hours)
+                              } 
+
+  def  takehomesalary(normal: Int, ot:Int) ={
+              print ("your takehome salary is" +(normal+ot)*0.9) 
+                     } 
+
+takehomesalary( normal(40),ot(10))
+}
