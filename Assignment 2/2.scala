f98dc5a4b6220258bd035806a43af1c4318b143c
WarunaPrabashwara <55828398+WarunaPrabashwara@users.noreply.github.com>
2020-05-29 18:47:02 +0530

    Update 2.scala

--- Assignment 2/2.scala
+++ Assignment 2/2.scala
@@ -1,40 +1,8 @@
-
 object takehomesalary extends App {
+  for (i <- 1 to 10) profit(i)
 
-  def best_profit() = {
-    var For_100: Int = 100 * (20 - 3);
-    var For_120: Int = 120 * (15 - 3);
-    var For_140: Int = 140 * (10 - 3);
-
-    if (cost(For_100) > cost(For_120) && cost (For_100) > cost(For_140)) {
-      print("best price is 20 ")
-      print(
-        " profit from 100,120,140 people respectively"
-          + cost(For_100) + "," +
-          cost(For_120) + "," +
-          cost(For_140)
-      )
-    } else {
-      if (cost(For_120) > cost(For_140)) {
-        print("best ticket price is 15 ")
-        print(
-          " profit from 100,120,140 people respectively"
-            + cost(For_100) + "," +
-            cost(For_120) + "," +
-            cost(For_140)
-        )
-      } else {
-        print("best ticket price is 10 ")
-        print(
-          " profit from 100,120,140 people respectively"
-            + cost(For_100) + "," +
-            cost(For_120) + "," +
-            cost(For_140)
-        )
-      }
-    }
+  def profit(i: Int) = {
+    printf("profit when ticket price is  %d is %d \n",(i * 5),(180 - 20 * i) * (5 * i - 3) - 500 )
   }
-  def cost(k: Int) = (k - 500);
 
-  best_profit()
 }
