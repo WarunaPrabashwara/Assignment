771813e8c92197e91d5a254c763e21cd563dfe84
WarunaPrabashwara <55828398+WarunaPrabashwara@users.noreply.github.com>
2020-05-29 11:25:02 +0530

    Create 6th question.scala

---
+++ Assignment 3/6th question.scala
@@ -0,0 +1,20 @@
+object 6 extends App{
+
+val a=scala.io.StdIn.readInt()
+
+  fib(a) 
+
+    def fib(n: Int) = {
+
+ fib(0)=0
+
+fib(1)=1
+
+      if (n<=1) print n
+
+     print ( fib(n-1)+fib(n-2))
+
+  } 
+
+}
+
