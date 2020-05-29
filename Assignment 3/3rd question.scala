3d4f25cd58627dc84d7e9479bcf12f4e03d01290
WarunaPrabashwara <55828398+WarunaPrabashwara@users.noreply.github.com>
2020-05-29 11:22:48 +0530

    Create 3rd question.scala

---
+++ Assignment 3/3rd question.scala
@@ -0,0 +1,11 @@
+object 3 extends App{
+print "enter a positive integer" 
+val a=scala.io.StdIn.readInt()
+print sum(a) 
+    def sum(c: Int):Int = {
+      if (n != 0)
+        return n + addNumbers(n - 1)
+    else
+        return n
+  } 
+}
