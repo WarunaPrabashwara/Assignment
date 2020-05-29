353def3d69ae30f0661e0c6102293c4be3485cf7
WarunaPrabashwara <55828398+WarunaPrabashwara@users.noreply.github.com>
2020-05-29 13:50:42 +0530

    Update 4th question.scala

--- Assignment 3/4th question.scala
+++ Assignment 3/4th question.scala
@@ -1,12 +1,10 @@
-object 4 extends App{
-val a=scala.io.StdIn.readInt()
-     def oddeven(n: Int) = {
-if (n < 2)
-        return (n % 2 == 0)
-    return (oddeven(n - 2))
-         } 
-if(oddeven(a)==true)
-      print "Number is even!"
-else:
-      print "Number is odd!"
+
+object q4 extends App {
+  val a = scala.io.StdIn.readInt()
+  def oddeven(n: Int): Boolean = if (n < 2) (n % 2 == 0) else (oddeven(n - 2))
+
+  if (oddeven(a) == true)
+    print("Number is even!")
+  else
+    print("Number is odd!")
 }
