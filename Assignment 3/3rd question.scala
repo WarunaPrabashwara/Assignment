b2b41eb47af6c9a788ec42bfa3b0ee48fc4d89af
WarunaPrabashwara <55828398+WarunaPrabashwara@users.noreply.github.com>
2020-05-30 17:26:02 +0530

    Update 3rd question.scala

--- Assignment 3/3rd question.scala
+++ Assignment 3/3rd question.scala
@@ -4,6 +4,7 @@ def sum(n:Int):Int = n match {
      case n if (n>=1) => n+sum(n-1)
      case _ => return 0 ;
 }
-print("enter a number to take the sum upto it") 
+print("enter a number to take the sum upto it")
+val a = scala.io.StdIn.readInt()
 println("sum is:" +sum(a)) ;
-} 
+}
