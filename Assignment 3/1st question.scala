22071a82b1a4d33d26584bcef1f8acfd9333f705
WarunaPrabashwara <55828398+WarunaPrabashwara@users.noreply.github.com>
2020-05-30 13:31:02 +0530

    Update 1st question.scala

--- Assignment 3/1st question.scala
+++ Assignment 3/1st question.scala
@@ -1,13 +1,13 @@
+object q1 extends App {
+print("enter the number to see if it is a prime: ") 
+val c = scala.io.StdIn.readInt()
+isprime(c, 2) 
 
-object 1 extends App{
-print "enter the number to see if it is a prime"
-val a=scala.io.StdIn.readInt()
-  isprime(a, 2) 
-    def isprime(c: Int,d :Int) = {
-           if(c<2) print "false" 
-           if (c==2) print "true" 
-            if(c%d ==0) print "false" 
-           if (d*d>c) print "true" 
-           isprime( n, d+1)
-  } 
+def isprime(c: Int,d :Int):Any = {
+if (c<2) print("false") 
+else if (c==2) print("true") 
+else if (c%d ==0) print("false") 
+else if (d*d>c) print("true") 
+else isprime(c, d+1)
+} 
 }
