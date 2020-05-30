740427debc723227f624ee899d306d40e616cc2a
WarunaPrabashwara <55828398+WarunaPrabashwara@users.noreply.github.com>
2020-05-30 17:50:17 +0530

    Update 2nd question.scala

--- Assignment 3/2nd question.scala
+++ Assignment 3/2nd question.scala
@@ -1,14 +1,18 @@
-object 2 extends App{
-print "enter the number to take prime numbers upto that" 
-val a=scala.io.StdIn.readInt()
-  printprime(a) 
-def printprime(n :Int) ={
-      for(i:Int=2 ; i<n-1 ;i ++) 
-          if(isprime(i))  print ( i + " ") 
-def isprime(n :Int):Boolean ={
-      if(n<=1) return false ;
-      for( i :Int =2 ;i<n;i++)  if (n%i ==0) return false 
-return true
+
+object q2 extends App {
+print("enter the number to see prime numbers upto it: ") 
+var c = scala.io.StdIn.readInt()
+var k:Int = c-1
+for (i<- 2 to c) {
+if(isprime(k, 2) ) println (k) 
+k=k-1
 } 
-}   
+def isprime(c: Int,d :Int):Boolean = {
+if (c<2) return false
+else if (c==2) return true
+else if (c%d ==0) return false
+else if (d*d>c) return true
+else isprime(c, d+1)
+} 
 }
+
