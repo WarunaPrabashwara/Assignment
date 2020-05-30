52a96c9f1b67a9bbe97f5bf58fa701810e1d0cd1
WarunaPrabashwara <55828398+WarunaPrabashwara@users.noreply.github.com>
2020-05-30 21:18:20 +0530

    Update 6th question.scala

--- Assignment 3/6th question.scala
+++ Assignment 3/6th question.scala
@@ -1,20 +1,19 @@
-object 6 extends App{
+object q6 extends App {
+def fib(n:Int) :Int = n match {
+    case 0 => 0
+    case x if(x==1) =>n
+    case x => fib(x-1) + fib(x-2)
+} 
 
-val a=scala.io.StdIn.readInt()
+def printfib(n1:Int, n:Int=0):Any ={
+ if(n1 >= n) {
+print(fib(n) +" ") 
+printfib(n1, n+1)
+} 
+} 
 
-  fib(a) 
-
-    def fib(n: Int) = {
-
- fib(0)=0
-
-fib(1)=1
-
-      if (n<=1) print n
-
-     print ( fib(n-1)+fib(n-2))
-
-  } 
+print ("Enter number to see the fibonacci numbers upto it") 
+var num: Int = scala.io.StdIn.readInt() 
+printfib(num) 
 
 }
-
